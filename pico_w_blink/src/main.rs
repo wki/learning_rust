8df25eb3c76d1f33f1c33e09401460a7681cdf8f
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2025-07-18 19:44:00 +0200

    added 128x64 display

--- pico_w_blink/src/main.rs
+++ pico_w_blink/src/main.rs
@@ -10,9 +10,18 @@ use embassy_rp::i2c::{self, Async, Config};
 use embassy_rp::peripherals::{DMA_CH0, PIO0, I2C1};
 use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
 use embassy_time::{Duration, Timer};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+    primitives::{Rectangle, Circle},
+};
+use embedded_graphics::primitives::{Line, PrimitiveStyle, PrimitiveStyleBuilder};
+use sh1106::Builder;
+use sh1106::prelude::GraphicsMode;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
-// use embedded_hal_1::i2c::I2c;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
@@ -106,7 +115,7 @@ async fn main(spawner: Spawner) {
     info!("Program start");
 
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_22, Level::Low);
+    let mut led = Output::new(p.PIN_15, Level::Low);
 
     // Configure PIO and CYW43
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
@@ -144,28 +153,76 @@ async fn main(spawner: Spawner) {
     // Configure display
     let config = Config::default();
 
-    let mut i2c = i2c::I2c::new_async(p.I2C1, p.PIN_3, p.PIN_2, Irqs, config);
-    lcd_init(&mut i2c).await;
-    lcd_clear(&mut i2c).await;
-    // lcd_cursor(&mut i2c, 0, 0).await;
-    lcd_message(&mut i2c, "Welcome to Rust").await;
+    let i2c = i2c::I2c::new_async(p.I2C1, p.PIN_3, p.PIN_2, Irqs, config);
+    // lcd_init(&mut i2c).await;
+    // lcd_clear(&mut i2c).await;
+    // // lcd_cursor(&mut i2c, 0, 0).await;
+    // lcd_message(&mut i2c, "Welcome to Rust").await;
 
-    let mut i:u8 = 0;
+    // init sh1106 128x64 display
+    let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Line::new(Point::new(8, 32 + 16), Point::new(8 + 16, 32 + 16))
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(&mut display)
+        .unwrap();
+
+    Line::new(Point::new(8, 32 + 16), Point::new(8 + 8, 32))
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(&mut display)
+        .unwrap();
+
+    Line::new(Point::new(8 + 16, 32 + 16), Point::new(8 + 8, 32))
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(&mut display)
+        .unwrap();
+
+    Rectangle::with_corners(Point::new(48, 32), Point::new(48 + 16, 32 + 16))
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(&mut display)
+        .unwrap();
+
+    Circle::new(Point::new(88, 32), 16)
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    let mut i:i32 = 0;
     loop {
-        lcd_cursor(&mut i2c, 1, 0).await;
-        lcd_message(&mut i2c, "Counter: ").await;
-        lcd_char(&mut i2c, 0x30 + i / 10).await;
-        lcd_char(&mut i2c, 0x30 + i % 10).await;
-
+    //     lcd_cursor(&mut i2c, 1, 0).await;
+    //     lcd_message(&mut i2c, "Counter: ").await;
+    //     lcd_char(&mut i2c, 0x30 + i / 10).await;
+    //     lcd_char(&mut i2c, 0x30 + i % 10).await;
+    
         info!("external LED on, onboard LED off!");
         led.set_high();
         control.gpio_set(0, false).await;
         Timer::after(Duration::from_secs(1)).await;
-
+    
         info!("external LED off, onboard LED on!");
         led.set_low();
         control.gpio_set(0, true).await;
         Timer::after(Duration::from_secs(1)).await;
+
+        info!("i is {}", i);
 
         i = if i<99 {i+1} else {0};
     }
