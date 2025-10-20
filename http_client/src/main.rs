869d5a5c9b104294caeab8ce16dd21838b95f64a
Wolfgang Kinkeldei <wkinkeldei@outlook.de>
2025-10-20 19:22:32 +0200

    added simple http client demo

---
+++ http_client/src/main.rs
@@ -0,0 +1,17 @@
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // curl -v -H 'Accept-Language:de' 'http://wttr.in/erlangen?0QT'
+
+    let resp = reqwest::Client::new()
+        .get("http://wttr.in/erlangen?0QT")
+        .header("Accept", "text/plain")
+        .header("Accept-Language", "de-DE")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{resp:#?}");
+    Ok(())
+}
