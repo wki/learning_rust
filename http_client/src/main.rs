fdf2d013e1993b5233105eca85a79f734a456ce6
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2025-10-22 11:07:31 +0200

    added header to wttr.in to retrieve plain-text

--- http_client/src/main.rs
+++ http_client/src/main.rs
@@ -7,11 +7,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .get("http://wttr.in/erlangen?0QT")
         .header("Accept", "text/plain")
         .header("Accept-Language", "de-DE")
+        .header("User-Agent", "curl/8.1.7")
         .send()
         .await?
         .text()
         .await?;
 
-    println!("{resp:#?}");
+    println!("{resp}");
     Ok(())
 }
